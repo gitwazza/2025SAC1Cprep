fad50924bb523543938d57b985c10b600b25541e
Warren Sutton <sutton.warren@live.com.au>
2024-04-11 22:30:39 +1000

    2nd commit from Pycharm

--- first.py
+++ first.py
@@ -1,1 +1,11 @@
 print("getting started!")
+
+
+class Booking:
+
+    def __init__(self, num):
+        self.num = int(num)
+        print(f"booking made for {num} people")
+
+
+my_booking = Booking(3)
